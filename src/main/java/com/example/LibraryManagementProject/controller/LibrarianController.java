b3bb1d41b11ced1943693bda431d98a3664736b3
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-01-11 00:23:28 +0000

    added controller

--- src/main/java/com/example/LibraryManagementProject/controller/LibrarianController.java
+++ src/main/java/com/example/LibraryManagementProject/controller/LibrarianController.java
@@ -1,7 +1,36 @@
 package com.example.LibraryManagementProject.controller;
 
+import com.example.LibraryManagementProject.model.Book;
+import com.example.LibraryManagementProject.service.BookService;
+import com.example.LibraryManagementProject.service.LibrarianService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin")
 public class LibrarianController {
+
+    @Autowired
+    LibrarianService librarianService;
+
+    @Autowired
+    Book book;
+
+    @Autowired
+    BookService bookService;
+
+    public LibrarianController(LibrarianService librarianService) {
+        this.librarianService = librarianService;
+    }
+
+    @PostMapping("/create")
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createNewBook(book);
+    }
+
+
+
 }
--- src/main/java/com/example/LibraryManagementProject/service/LibrarianService.java
+++ src/main/java/com/example/LibraryManagementProject/service/LibrarianService.java
@@ -1,7 +1,29 @@
 package com.example.LibraryManagementProject.service;
 
+import com.example.LibraryManagementProject.model.Book;
+import com.example.LibraryManagementProject.model.Librarian;
+import com.example.LibraryManagementProject.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LibrarianService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public LibrarianService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    //add new book
+    public Book createNewBook(Book book) {
+        bookRepository.insert(book);
+        return book;
+
+    }
+
 }

0c085e1f47e98e1cd96755a6984b34b4e1b4a948
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-01-12 11:12:11 +0000

    created controller for create and delete method for admin

--- src/main/java/com/example/LibraryManagementProject/controller/LibrarianController.java
+++ src/main/java/com/example/LibraryManagementProject/controller/LibrarianController.java
@@ -1,6 +1,7 @@
 package com.example.LibraryManagementProject.controller;
 
 import com.example.LibraryManagementProject.model.Book;
+import com.example.LibraryManagementProject.repository.BookRepository;
 import com.example.LibraryManagementProject.service.BookService;
 import com.example.LibraryManagementProject.service.LibrarianService;
 import org.slf4j.LoggerFactory;
@@ -13,35 +14,39 @@ import java.util.logging.Logger;
 @RequestMapping("/admin")
 public class LibrarianController {
 
-    Logger logger = (Logger) LoggerFactory.getLogger(LibrarianController.class);
+
 
     @Autowired
     LibrarianService librarianService;
 
-    @Autowired
+
     Book book;
 
     @Autowired
     BookService bookService;
 
+    @Autowired
+    BookRepository bookRepository;
+
     public LibrarianController(LibrarianService librarianService) {
         this.librarianService = librarianService;
     }
+//    Logger logger = (Logger) LoggerFactory.getLogger(LibrarianController.class);
 
     @PostMapping("/create")
     public Book createBook(@RequestBody Book book) {
-        logger.info("Book Created");
+//        logger.info("Book Created");
         return bookService.createNewBook(book);
     }
 
-    @DeleteMapping("/remove-book/{id}")
-    public String deleteBook(@PathVariable("id") Long id, Book book) {
-        bookService.deleteBook(id);
-
-        book.addAttribute("book", bookService.getAllBooks());
-        logger.info("Book Deleted");
-        return "redirect:/books";
+    @DeleteMapping("/remove-book")
+    public String deleteBook(@RequestParam int bookISBNNumber) {
+        librarianService.deleteBook(bookISBNNumber);
+//  logger.info("Book Deleted");
+        return "redirect:/admin/remove-books";
     }
+
+
 
 
 

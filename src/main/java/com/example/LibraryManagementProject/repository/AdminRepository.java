661d36e5b23b93c9f378e4be7459c4e7aaabfd65
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-01-24 15:30:47 +0000

    User controller test - TDD for checkin and checkout method

--- src/main/java/com/example/LibraryManagementProject/repository/AdminRepository.java
+++ src/main/java/com/example/LibraryManagementProject/repository/AdminRepository.java
@@ -4,6 +4,9 @@ import com.example.LibraryManagementProject.model.Admin;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Admin repository class.
+ */
 @Repository
 public interface AdminRepository extends MongoRepository<Admin, String> {
 }
--- src/test/java/com/example/LibraryManagementProject/controller/UserControllerTest.java
+++ src/test/java/com/example/LibraryManagementProject/controller/UserControllerTest.java
@@ -6,22 +6,30 @@ import com.example.LibraryManagementProject.model.User;
 import com.example.LibraryManagementProject.repository.BookRepository;
 import com.example.LibraryManagementProject.repository.LibraryRepository;
 import com.example.LibraryManagementProject.repository.UserRepository;
+import com.example.LibraryManagementProject.requestbody.CheckInBookRequestBody;
+import com.example.LibraryManagementProject.requestbody.CheckoutBookRequestBody;
 import com.example.LibraryManagementProject.service.BookService;
 import com.example.LibraryManagementProject.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -37,33 +45,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 @AutoConfigureMockMvc
+@SpringBootTest
+@ContextConfiguration
 public class UserControllerTest {
 
-    @MockBean
-    private BookRepository bookRepository;
-
-    @MockBean
-    private LibraryRepository libraryRepository;
-
-    @Mock
-    private UserRepository userRepository;
-
-    @Mock
-    private Book book;
-
-    @MockBean
-    private UserController userController;
-
     @Autowired
-    private UserService userService;
+    MockMvc mockMvc;
 
     @Mock
-    private RequestAttributes attributes;
+    UserService userService;
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Mock
+    User user;
+
+    @Mock
+    Book book;
+
+    @Mock
+    BookRepository bookRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    @MockBean
+    LibraryRepository libraryRepository;
+
+    @MockBean
+    UserController userController;
+
+    @Mock
+    RequestAttributes attributes;
+
 
     @Before
     public void setUp() {
@@ -71,45 +84,55 @@ public class UserControllerTest {
         this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
     }
 
+    /**
+     * book checked out successfully test
+     */
     @Test
     @DisplayName("Test Should Pass If Book Has Been Checked out successfully Library")
     public void bookCheckOut_SuccessTest() throws Exception {
-        Library library = new Library();
-        library.setUserEmailAddress("tiff@lib.com");
-        library.setBookISBN(12345678);
-        library.setIssueDate(LocalDate.parse("2022-09-21"));
-        library.setDueDate(LocalDate.parse("2022-10-01"));
-        LibraryRepository libraryRepository = mock(LibraryRepository.class);
-        when(libraryRepository.insert((Library) org.mockito.Mockito.any())).thenReturn(library);
-        User user = new User();
-        user.setUserEmailAddress("tiff@lib.com");
-        when(userRepository.findUserByUserEmailAddress("tiff@lib.com")).thenReturn(user);
-        UserController userController = new UserController(new UserService(libraryRepository));
-        Library library1 = new Library();
-        library1.setUserEmailAddress("tiff@lib.com");
-        library1.setBookISBN(12345678);
-        library.setIssueDate(LocalDate.parse("2022-09-21"));
-        library1.setDueDate(LocalDate.parse("2022-10-01"));
-        assertSame(library1, userController.checkOutBook(library1, 7));
-        verify(libraryRepository).insert((Library) org.mockito.Mockito.any());
-
-        }
-
+        //create new object of checkOutBookRequest
+        CheckoutBookRequestBody checkoutBookRequestBody = new CheckoutBookRequestBody();
+        //setting the fields
+        checkoutBookRequestBody.setUserEmailAddress("tiff@lib.com");
+        checkoutBookRequestBody.setBookIsbn(12345678);
+        //mock when checkoutbook method is called, this should return
+        Mockito.when(userService.checkOutBook(checkoutBookRequestBody)).thenReturn("tiff@lib.com has borrowed one copy of 12345678!");
+        //what the json body should look like
+        String jsonBody = "{\"userEmailAddress\":\"tiff@lib.com\", " +
+                "\"bookISBN\":\"12345678\"," +
+                "\"borrowForDays\":\"7\"}";
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .post("/user/checkout")
+                                .content(jsonBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
 
 
-//    @Test
-//    @DisplayName("Test Should Pass If Book Has Been Returned to the Library")
-//    public void bookReturned_SuccessTest() throws Exception {
-//        doNothing().when(userService).checkInBook("tiffany@lib.com", 12345678);
-//        mockMvc.perform(MockMvcRequestBuilders
-//                .post("/user/checkin")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON));
-//
-//    }
-//
+    @Test
+    @DisplayName("Test Should Pass If Book Has Been Returned to the Library")
+    public void bookReturned_SuccessTest() throws Exception {
+        CheckInBookRequestBody checkInBookRequestBody = new CheckInBookRequestBody();
+        checkInBookRequestBody.setUserEmailAddress("libby@lib.com");
+        checkInBookRequestBody.setBookIsbn(87654321);
+        Mockito.when(userService.CheckInBook(checkInBookRequestBody)).thenReturn("libby@lib.com has returned copy of 12345678!");
+        String jsonBody1 = "{\"userEmailAddress\":\"libby@lib.com\", " +
+                "\"bookISBN\":\"87654321\"";
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .put("/user/checkin")
+                                .content(jsonBody1)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
 
 
+    }
 }
+
+
+
 
 

1f0fbf8af8d5e81142943cea125b2fdb87df1154
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-01-24 16:16:28 +0000

    removed unused imports

--- src/main/java/com/example/LibraryManagementProject/controller/UserController.java
+++ src/main/java/com/example/LibraryManagementProject/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.example.LibraryManagementProject.controller;
 
-import com.example.LibraryManagementProject.model.Book;
-import com.example.LibraryManagementProject.model.Library;
-import com.example.LibraryManagementProject.repository.BookRepository;
+import com.example.LibraryManagementProject.model.User;
 import com.example.LibraryManagementProject.repository.UserRepository;
-import com.example.LibraryManagementProject.service.BookService;
+import com.example.LibraryManagementProject.requestbody.CheckInBookRequestBody;
+import com.example.LibraryManagementProject.requestbody.CheckoutBookRequestBody;
 import com.example.LibraryManagementProject.service.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -20,18 +20,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
-  @Autowired
-  UserService userService;
 
+  @Autowired
+  private final UserService userService;
+
+
+  @Autowired
   UserRepository userRepository;
 
-  Book book;
-
-  @Autowired
-  BookService bookService;
-
-  @Autowired
-  BookRepository bookRepository;
 
   public UserController(UserService userService) {
     this.userService = userService;
@@ -40,18 +36,33 @@ public class UserController {
   Logger logger = LoggerFactory.getLogger(BookController.class);
 
   /**
-   * Put request method to borrow books to the user.
+   * Post request method to borrow books to the user.
    */
   @PostMapping("/checkout")
-  public String checkOutBook(@RequestBody Library library, int borrowForDays) {
+  @ResponseStatus(HttpStatus.CREATED)
+  public String checkOutBook(@RequestBody CheckoutBookRequestBody checkoutBookRequestBody) {
     logger.info("Book successfully checked out");
-    return userService.checkOutBook(library,borrowForDays);
+    return userService.checkOutBook(checkoutBookRequestBody);
+  }
+
+  /**
+   * Post request method to create a user.
+   */
+  @PostMapping("/createUser")
+  public String createBook(@RequestBody User user) {
+    logger.info("User Created");
+    return userService.createUser(user);
+
+  }
+
+  @PutMapping("/checkin")
+  @ResponseStatus(HttpStatus.CREATED)
+  public String CheckInBook(@RequestBody CheckInBookRequestBody checkInBookRequestBody) {
+    logger.info("Book successfully checked in");
+    return userService.checkInBook(checkInBookRequestBody);
   }
 }
 
-//  @PutMapping("/checkin")
-//  public String returnBook(@RequestBody Library library) {
-//   return userService.checkInBook(library);
-//  }
+
 
 
--- src/main/java/com/example/LibraryManagementProject/service/UserService.java
+++ src/main/java/com/example/LibraryManagementProject/service/UserService.java
@@ -6,12 +6,13 @@ import com.example.LibraryManagementProject.model.User;
 import com.example.LibraryManagementProject.repository.BookRepository;
 import com.example.LibraryManagementProject.repository.LibraryRepository;
 import com.example.LibraryManagementProject.repository.UserRepository;
+import com.example.LibraryManagementProject.requestbody.CheckInBookRequestBody;
+import com.example.LibraryManagementProject.requestbody.CheckoutBookRequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
+
 
 /**
  * User service class.
@@ -20,30 +21,41 @@ import java.util.Date;
 @Service
 public class UserService {
 
+  @Autowired
   LibraryRepository libraryRepository;
 
+  @Autowired
   UserRepository userRepository;
 
+  @Autowired
   BookRepository bookRepository;
 
-  Library library;
-
-  User user;
-
-
-  public UserService(LibraryRepository libraryRepository) {
+  /**
+   * constructor generated for the user service class.
+   */
+  public UserService(LibraryRepository libraryRepository, UserRepository userRepository, BookRepository bookRepository) {
     this.libraryRepository = libraryRepository;
+    this.userRepository = userRepository;
+    this.bookRepository = bookRepository;
   }
 
+  /**
+   * create user method class.
+   */
+  public  String createUser(User user) {
+    userRepository.insert(user);
+    return "user has been created";
+
+  }
 
   /**
    * method to allow a user.
    * to check out a book.
    */
-  public String checkOutBook(Library library, int borrowForDays) {
+  public String checkOutBook(CheckoutBookRequestBody checkoutBookRequestBody) {
 
-    User user = userRepository.findUserByUserEmailAddress(library.getUserEmailAddress());
-    Book book = bookRepository.findBookByBookISBN(library.getBookISBN());
+    User user = userRepository.findUserByUserEmailAddress(checkoutBookRequestBody.getUserEmailAddress());
+    Book book = bookRepository.findBookByBookISBN(checkoutBookRequestBody.getBookIsbn());
 
 
     if (book.getBookQuantity() < 1) {
@@ -52,31 +64,31 @@ public class UserService {
     book.borrowBook();
     bookRepository.save(book);
     LocalDate currentDate = LocalDate.now();
-    LocalDate overdueDate = currentDate.plusDays(borrowForDays);
+    LocalDate overdueDate = currentDate.plusDays(checkoutBookRequestBody.getBorrowForDays());
+    Library library = new Library();
     library.setIssueDate(currentDate);
     library.setDueDate(overdueDate);
     libraryRepository.save(library);
     return user.getUserEmailAddress() + " has borrowed one copy of \"" + book.getBookTitle() + "\"!";
   }
-}
-//  /**
-//   * method to allow a user.
-//   * to check in a book.
-//   */
-//  public boolean checkInBook(Library library) {
-//
-//    Library library1 = libraryRepository.findById(library.getBorrowId()).get();
-//    Books book = libraryRepository.findById(library.getBookISBN()).get();
-//
-//    book.returnBook();
-//    booksRepository.save(book);
-//
-//    Date currentDate = new Date();
-//    borrowBook.setReturnDate(currentDate);
-//    return borrowRepository.save(borrowBook);
-//
-//  }
-//}
 
 
+  /**
+   * method to allow a user.
+   * to check out a book.
+   */
+  public String checkInBook(CheckInBookRequestBody checkInBookRequestBody) {
 
+    User user = userRepository.findUserByUserEmailAddress(checkInBookRequestBody.getUserEmailAddress());
+    Book book = bookRepository.findBookByBookISBN(checkInBookRequestBody.getBookIsbn());
+
+    book.returnBook();
+    bookRepository.save(book);
+    LocalDate currentDate = LocalDate.now();
+    Library library = new Library();
+    library.setReturnDate(currentDate);
+    libraryRepository.save(library);
+    return user.getUserEmailAddress() + " has returned copy of \"" + book.getBookTitle() + "\"!";
+
+  }
+  }

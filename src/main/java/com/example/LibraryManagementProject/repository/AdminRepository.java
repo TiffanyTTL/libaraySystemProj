c9e77913e87aa8204ef9bdfe8cc27e7acda3538b
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-01-24 18:03:57 +0000

    admin repo updated

--- src/main/java/com/example/LibraryManagementProject/repository/AdminRepository.java
+++ src/main/java/com/example/LibraryManagementProject/repository/AdminRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AdminRepository extends MongoRepository<Admin, String> {
+
+    Admin findAdminByAdminEmailAddress(String adminEmailAddress);
 }

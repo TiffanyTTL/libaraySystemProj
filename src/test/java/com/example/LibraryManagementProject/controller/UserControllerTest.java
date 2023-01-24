ace650067bc0c67cca2d8a94cd59e871114dfe93
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-01-24 15:50:58 +0000

    deleted unused imports

--- src/test/java/com/example/LibraryManagementProject/controller/UserControllerTest.java
+++ src/test/java/com/example/LibraryManagementProject/controller/UserControllerTest.java
@@ -1,27 +1,21 @@
 package com.example.LibraryManagementProject.controller;
 
 import com.example.LibraryManagementProject.model.Book;
-import com.example.LibraryManagementProject.model.Library;
 import com.example.LibraryManagementProject.model.User;
 import com.example.LibraryManagementProject.repository.BookRepository;
 import com.example.LibraryManagementProject.repository.LibraryRepository;
 import com.example.LibraryManagementProject.repository.UserRepository;
 import com.example.LibraryManagementProject.requestbody.CheckInBookRequestBody;
 import com.example.LibraryManagementProject.requestbody.CheckoutBookRequestBody;
-import com.example.LibraryManagementProject.service.BookService;
 import com.example.LibraryManagementProject.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -29,19 +23,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
-
-import java.sql.Date;
-import java.text.DateFormat;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -117,9 +101,9 @@ public class UserControllerTest {
         CheckInBookRequestBody checkInBookRequestBody = new CheckInBookRequestBody();
         checkInBookRequestBody.setUserEmailAddress("libby@lib.com");
         checkInBookRequestBody.setBookIsbn(87654321);
-        Mockito.when(userService.CheckInBook(checkInBookRequestBody)).thenReturn("libby@lib.com has returned copy of 12345678!");
+        Mockito.when(userService.checkInBook(checkInBookRequestBody)).thenReturn("libby@lib.com has returned copy of 12345678!");
         String jsonBody1 = "{\"userEmailAddress\":\"libby@lib.com\", " +
-                "\"bookISBN\":\"87654321\"";
+                "\"bookISBN\":\"87654321\"}";
         mockMvc.perform(
                         MockMvcRequestBuilders
                                 .put("/user/checkin")

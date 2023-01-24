0b9ab8789108dedb9e2bf5f7ae20cd50836b42ca
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-01-24 15:35:04 +0000

    added a few annotations

--- src/main/java/com/example/LibraryManagementProject/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/LibraryManagementProject/LibraryManagementSystemApplication.java
@@ -4,10 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**spring boot library management application.
  */
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @OpenAPIDefinition(info = @Info(title = "Library Management API", version = "2.0",
 		description = "library management microservice"))
 public class LibraryManagementSystemApplication  {
